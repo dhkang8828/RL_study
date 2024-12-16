9945c15355463d9c7c5c4cf97d836e61d38d1500
dhkang8828 <dh.kang8828@gmail.com>
2024-12-16 23:49:03 +0900

    Add comment

--- Policy_Evaluation_Ex1/policy_evaluation.py
+++ Policy_Evaluation_Ex1/policy_evaluation.py
@@ -9,22 +9,27 @@ def policy_evaluation(env, policy):
     gamma = 0.9
     delta = 1e-3
     Delta = delta
+    ## state-space에 대한 value-vector
+    ## v = [v0, v1, ... ,vs]
     value_vector = np.zeros([len(env.state_space)])
 
     loop_count = 0
     while Delta >= delta:
         Delta = 0
         new_value_vector = np.zeros([len(env.state_space)])
+        ## state-space에 index 부여
         for i_s, s in enumerate(env.state_space):
             v_s = 0
             # sum over action
             for a in env.action_space:
                 # sum over next state
                 for i_s_next, s_next in enumerate(env.state_space):
+                    ## policy (i번째 state에서 action a를 선택할 확률)
                     pi_a = policy[i_s][a]
                     p_s_next = env.transition_probability(s, a, s_next)
                     reward = env.reward(s, a, s_next)
 
+                    ## Bellman Equation for Value-Function
                     v_s = v_s + p_s_next * pi_a * (reward + gamma * value_vector[i_s_next])
             new_value_vector[i_s] = v_s
             value_delta = abs(new_value_vector[i_s] - value_vector[i_s])
