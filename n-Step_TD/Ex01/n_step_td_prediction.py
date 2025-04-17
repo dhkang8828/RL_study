d294cc6844eccfcc5d17eeac246633307a6d836c
donghwa kang <dh.kang8828@gmail.com>
2025-04-17 22:59:24 +0900

    feat: implement n-step TD Ex01

---
+++ n-Step_TD/Ex01/n_step_td_prediction.py
@@ -0,0 +1,27 @@
+## n_step_td_prediction.py
+
+import numpy as np
+import matplotlib.pyplot as plt
+from environment import Env
+
+gamma = 0.9
+alpha_init = 2e-1
+k_alpha = 2.5e-1
+
+def get_state_index(state_space, state):
+    for i_s, s in enumerate(state_space):
+        if (s == state).all():
+            return i_s
+    assert False, "Could not find state in state space"
+    
+def calc_return(gamma, rewards):
+    n = len(rewards)
+    rewards = np.array(rewards)
+    gammas = gamma * np.ones([n])
+    powers = np.arange(n)
+    
+    power_of_gammas = np.power(gammas, powers)
+    ## [r, gamma *r , gamma^2 * r...]
+    discounted_rewards = rewards * power_of_gammas 
+    g = np.sum(discounted_rewards)
+    return g
